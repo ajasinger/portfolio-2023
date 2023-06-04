4ea2be6cb9b3a81734b31adb8fe94037ffca4c0e
Aja Singer <ajasinger@gmail.com>
2023-06-03 20:25:03 -0400

    add Links component

---
+++ src/Components/Links.jsx
@@ -0,0 +1,13 @@
+const Links = () => {
+    return(
+        <div>
+            <a>LinkedIn</a>
+            <a>Resume</a>
+            <a>Writing</a>
+            <a>Substack</a>
+            <a>Twitter</a>
+        </div>
+    )
+}
+
+export default Links;

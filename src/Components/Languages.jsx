bc1d9bf755417ec127a6de1bf3c0c60522019c66
Aja Singer <ajasinger@gmail.com>
2023-06-03 20:31:57 -0400

    add languages component

---
+++ src/Components/Languages.jsx
@@ -0,0 +1,11 @@
+const Languages = () => {
+    return(
+        <div>
+            <h6>JavaScript</h6>
+            <h6>React</h6>
+            <h6>Next.js</h6>
+        </div>
+    )
+}
+
+export default Languages;

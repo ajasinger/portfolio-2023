d844e7c9a4139fc5714c3462843848e7a01e7e71
Aja Singer <ajasinger@gmail.com>
2023-06-03 20:21:58 -0400

    add basic header structural elements

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
 const Header = () => {
     return(
         <div>
-
+            <h1>Hi, I'm Aja</h1>
+            <h2>
+                a front-end developer working at the intersection of tech, 
+                design, and creativity.
+            </h2>
+            <p>longer bio....</p>
+            <button>GET IN TOUCH</button>
         </div>
     )
 }
